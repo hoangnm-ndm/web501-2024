006a5df8b9f632ae790385b86b7cf8769c3db474
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-04 17:27:11 +0700

    Buổi 3: register form

--- event.js
+++ event.js
@@ -1,9 +1,9 @@
-function hello() {
-  console.log("Xin chao!");
-}
+// function hello() {
+//   console.log("Xin chao!");
+// }
 
-var btn2Element = document.getElementsByClassName("btn2")[0];
-btn2Element.onclick = hello;
-var btn2Element3 = document.getElementsByClassName("btn2")[1];
-console.log(btn2Element3);
-btn2Element3.addEventListener("click", hello);
+// var btn2Element = document.getElementsByClassName("btn2")[0];
+// btn2Element.onclick = hello;
+// var btn2Element3 = document.getElementsByClassName("btn2")[1];
+// console.log(btn2Element3);
+// btn2Element3.addEventListener("click", hello);
