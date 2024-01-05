439a4b9a08d13c9f2a8e847083121b1245efdbdb
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-05 19:39:00 +0700

    Buổi 2: register form

--- event.js
+++ event.js
@@ -1,16 +1,18 @@
-var loginElement = document.getElementById("btn-login");
-console.log(loginElement);
-function login() {
-  console.log("Dang nhap thanh cong!");
-}
-loginElement.onclick = login;
+// var loginElement = document.getElementById("btn-login");
 
-// addEventListener
-var loginElement3 = document.getElementById("btn-login-3");
-loginElement3.addEventListener("click", login);
-
-// var myOto = {
-//   color: "red",
-//   run: function() {
-//   }
+// function login() {
+//   console.log("Dang nhap thanh cong!");
 // }
+
+// loginElement.onclick = login;
+
+// // addEventListener
+// var loginElement3 = document.getElementById("btn-login-3");
+// loginElement3.addEventListener("click", login);
+
+function register() {
+  /**
+   * Lay thong tin dang ky luu vao object userInfor
+   * Lay userInfor luu vao localStorage.
+   */
+}
