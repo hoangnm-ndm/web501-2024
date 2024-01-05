c5810a9b0cb23b85c9dc7e3432b2a202461a45b5
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-05 20:07:18 +0700

    Buổi 2: register form

--- event.js
+++ event.js
@@ -1,18 +1,21 @@
-// var loginElement = document.getElementById("btn-login");
-
-// function login() {
-//   console.log("Dang nhap thanh cong!");
-// }
-
-// loginElement.onclick = login;
-
-// // addEventListener
-// var loginElement3 = document.getElementById("btn-login-3");
-// loginElement3.addEventListener("click", login);
-
 function register() {
   /**
    * Lay thong tin dang ky luu vao object userInfor
    * Lay userInfor luu vao localStorage.
    */
+
+  // Buoc 1: Lay du lieu tu form:
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var confirmPass = document.getElementById("confirmPass").value;
+
+  // Buoc 2: Gom du lieu vao object
+  var userInfor = {
+    email,
+    password,
+    confirmPass,
+  };
+
+  // Buoc 3: Đưa vào localStorage.
+  localStorage.setItem("tenNguoiDung", JSON.stringify(userInfor));
 }
