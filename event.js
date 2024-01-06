cb1a866b84cfe30850b084aed1fa64d47b7df5a7
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-06 16:38:19 +0700

    Buổi 3: Validation register form

--- event.js
+++ event.js
@@ -1,22 +1,28 @@
-// function hello() {
-//   console.log("Xin chao!");
-// }
-
-// var btn2Element = document.getElementsByClassName("btn2")[0];
-// btn2Element.onclick = hello;
-// var btn2Element3 = document.getElementsByClassName("btn2")[1];
-// console.log(btn2Element3);
-// btn2Element3.addEventListener("click", hello);
-
 /**
  * Bước 1: Lấy giá trị từ các ô input ra.
  * Bước 2: Gom các giá trị vào object
  * Bước 3: Lưu object chứa thông tin này vào localStorage
  */
+// Cach tong quan:
+function validationForm({ ...args }) {}
 function register() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
   var confirmPass = document.getElementById("confirmPass").value;
+
+  // Kiem tra du lieu:
+  if (username === "" || password === "" || confirmPass === "") {
+    alert("khong duoc bo trong!");
+  }
+
+  if (password.length < 6) {
+    alert("Password phai dai hon 6 ky tu!");
+  }
+
+  if (password !== confirmPass) {
+    alert("Mat khau nhap lai khong khop!");
+  }
+
   // Buoc 2:
   var userInfor = {
     username: username,
@@ -26,4 +32,5 @@ function register() {
   // Buoc 3:
   // BOM = Browser Object Model
   localStorage.setItem("user", JSON.stringify(userInfor));
+  alert("Dang ky thanh cong!");
 }
