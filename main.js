7d6861c56848e6b8717706282bcafeb62b609126
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-11 17:47:11 +0700

    Buổi 4: Module

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import HomePage from "./src/pages/HomePage";
+import { datas, HomePage, HomePage2 } from "./src/pages/HomePage";
 import "./style.css";
 const app = document.getElementById("app");
 
 app.innerHTML = HomePage();
+
+console.log(datas);
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,4 +2,25 @@ const HomePage = () => {
   return `<h2>HomePage</h2>`;
 };
 
-export default HomePage;
+const HomePage2 = () => {
+  return `<h2>HomePage</h2>`;
+};
+
+export const datas = [
+  { id: "123", name: "Iphone 15 pro", price: 2000 },
+  { id: "123", name: "Iphone 15 pro", price: 2000 },
+  { id: "123", name: "Iphone 15 pro", price: 2000 },
+  { id: "123", name: "Iphone 15 pro", price: 2000 },
+];
+
+export { HomePage, HomePage2 };
+// export default HomePage;
+// export default HomePage2;
+
+/**
+ * 1. Chỉ Export default được 1 lần.
+ * 2. Ko export default trên chính dòng khai báo biến được
+ * 3. Nếu không export defaul thì có thể viết export cùng dòng khai báo biến.
+ * 4. Có thể gom các biến cần export vào một object
+ * 5. "type": "module"
+ */
