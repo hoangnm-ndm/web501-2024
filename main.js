ef4f0fe0be6569e64e0ccc9fbe0f431df101e01b
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-12 19:46:05 +0700

    Buổi 4: định tuyến

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 import "./style.css";
+import Navigo from "navigo";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    Hello!
-  </div>
-`;
+const router = new Navigo("/", {
+  linksSelector: "a",
+});
+const app = document.getElementById("app");
+const HomePage = () => {
+  return `
+  <h2>HomePage</h2>
+  `;
+};
+
+const AboutPage = () => {
+  return `
+  <h2>AboutPage</h2>
+  `;
+};
+
+const render = (target, content) => {
+  target.innerHTML = content();
+};
+
+router.on("/home", () => render(app, HomePage));
+router.on("/about", () => render(app, AboutPage));
+router.resolve();
