eab57fbe31319882233f14a83dd3a0acd609263a
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-10 16:55:27 +0700

    Lesson 3: Tim hieu du an

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <h1>Hello ca lop!</h1> */}
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// const app = document.getElementById("root");
+// const reactApp = ReactDOM.createRoot(app);
+// reactApp.render(<h1>Hello ca lop!</h1>);
