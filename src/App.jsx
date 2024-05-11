10fc734485ab86aed243fe42ccd6401e78fe4743
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-11 12:49:01 +0700

    Lesson 3: JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>Noi dung trang web</div>;
+  return (
+    // document.createElement("div")
+
+    // ! Cach viet thong thuong
+    // React.createElement(
+    //   "div",
+    //   { className: "infor" },
+    //   React.createElement(
+    //     "ul",
+    //     { className: "list" },
+    //     React.createElement("li", { className: "item" }, "item1"),
+    //     React.createElement("li", { className: "item" }, "item2"),
+    //     React.createElement("li", { className: "item" }, "item3")
+    //   )
+    // )
+
+    // ! Cach 2: viet = JSX (Javascript XML)
+    <div className="infor">
+      <ul className="list">
+        <li className="item">item1</li>
+        <li className="item">item2</li>
+        <li className="item">item3</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+// const app = document.getElementById("root");
+// const vitualDOM = ReactDOM.createRoot(app);
+// vitualDOM.render(<h1>Xin chao ca lop!</h1>);
