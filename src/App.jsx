cb42ecc0f249c5f4684931012cd10f00f2b67c71
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-11 12:13:20 +0700

    Lesson 3: Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import { Footer } from "./components/Footer";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
       {/* React Fragment */}
-      <Header />
+      {/* <Header />
       <main>
         <p>Noi dung trang web</p>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
-      <Footer />
+      <Footer /> */}
+      <Register />
     </>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function HomePage() {
+  console.log("Hello ca lop!");
+  return <div></div>;
+}
+
+export default HomePage;
