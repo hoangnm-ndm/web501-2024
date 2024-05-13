bdfadeea92ffc0724327bc7fa957d501ef70dad0
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-13 10:02:13 +0700

    Lesson 4: Header UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/shop">Shop</a>
+        </li>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
