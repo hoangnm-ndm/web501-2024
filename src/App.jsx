f9eaacd6a4e404bb7e42916903ea4bfebca65e05
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-13 11:28:44 +0700

    Lesson 4: Add login form

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <>
       <Header />
-      <HomePage />
+      {/* <HomePage /> */}
+      <Login />
       <Footer />
     </>
   );
