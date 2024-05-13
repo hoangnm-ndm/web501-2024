1d4fa6eb857a423dfcc9aec6656928759d0ca72f
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-13 17:31:35 +0700

    Lesson 4: NotfoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
+      {/* <Header />
+      <main className="container">
         <HomePage />
       </main>
-      <Footer />
+      <Footer /> */}
+      <NotFoundPage />
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 function HomePage() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello ca lop!</h1>
-      <button className="btn btn-primary">Nut bam tu bootstrap</button>
-    </div>
-  );
+  return <div>HomePage</div>;
 }
 
 export default HomePage;
