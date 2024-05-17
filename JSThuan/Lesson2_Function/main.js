e95b118d36755e7df8c54ee6df923e7b60579074
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-17 17:32:39 +0700

    Lesson 6: Props

--- JSThuan/Lesson2_Function/main.js
+++ JSThuan/Lesson2_Function/main.js
@@ -4,9 +4,9 @@
  * ! 2. Function ẩn danh (anonymous function)
  */
 
-function sum1(a, b) {
-  // console.log(a + b)
-  return a + b;
+function sum1(a, b = 10) {
+	// console.log(a + b)
+	return a + b;
 }
 
 console.log(sum1(2, 3));
@@ -14,12 +14,12 @@ console.log(sum1(2, 3));
 // ! Hàm ẩn danh
 // ! Expression function
 const sum2 = function () {
-  return a + b;
+	return a + b;
 };
 
 // ! Arrow function (ES6)
 const sum3 = (a, b) => {
-  return a + b;
+	return a + b;
 };
 
 // ! Hàm mũi tên viết gọn (khi có 1 câu lệnh duy nhất thì có thể bỏ qua dấu bao đóng của hàm {})
@@ -27,8 +27,8 @@ const sum4 = (a, b) => a + b;
 
 // ! Khi muốn trả về object hoặc array mà bỏ qua return thì cần bao đóng bằng dấu ()
 const showInfor = () => ({
-  name: "chu cuoi",
-  address: "moon",
+	name: "chu cuoi",
+	address: "moon",
 });
 console.log(sum3(10, 20));
 console.log(sum4(20, 30));
@@ -37,17 +37,17 @@ console.log(showInfor());
 // TODO: Viết 1 arrow function nhập vào số nguyên dương n, in ra mảng tất cả các số lẻ từ 1 đến n.
 
 const timSoLe = (n) => {
-  if (typeof n !== "number" || n <= 0) {
-    console.log("n khong hop le!");
-    return;
-  }
-  const result = [];
-  for (let i = 1; i <= n; i++) {
-    if (i % 2 !== 0) {
-      result.push(i);
-    }
-  }
-  console.log(`Ket qua cua phep tinh la: `, result);
+	if (typeof n !== "number" || n <= 0) {
+		console.log("n khong hop le!");
+		return;
+	}
+	const result = [];
+	for (let i = 1; i <= n; i++) {
+		if (i % 2 !== 0) {
+			result.push(i);
+		}
+	}
+	console.log(`Ket qua cua phep tinh la: `, result);
 };
 
 timSoLe(100);
--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,43 @@ import { Route, Routes } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import ShopPage from "./pages/ShopPage";
 
+const product = {
+	id: 1,
+	name: "Iphone 15",
+	price: 2000,
+};
+
+const product2 = {
+	id: 1,
+	name: "Iphone 16",
+	price: 2000,
+};
+
+// ! props = propeties
+function Detail(props) {
+	return (
+		<div>
+			<h2>{props.title}</h2>
+			<h1>Product Detail</h1>
+			<p>{props.data.id}</p>
+			<p>{props.data.name}</p>
+			<p>{props.data.price}</p>
+		</div>
+	);
+}
 function App() {
 	return (
 		<>
 			<Header />
 			<main className="container">
-				<Routes>
+				{/* {Detail(product)} */}
+				<Detail data={product} title="hello" />
+				{/* <Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/about" element={<AboutPage />} />
 					<Route path="/shop" element={<ShopPage />} />
 					<Route path="*" element={<NotFoundPage />} />
-				</Routes>
+				</Routes> */}
 			</main>
 			<Footer />
 		</>
