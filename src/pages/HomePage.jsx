830061e587f6c5cfc1330f7fb029c78bae485ed9
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-17 18:41:53 +0700

    Lesson 6: Danh sach san pham

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,26 @@ import React from "react";
 
 function HomePage({ products }) {
 	console.log(products);
-	return <div>{/* in ra trang danh sach san pham */}</div>;
+	return (
+		<>
+			<h1>San pham ban chay</h1>
+			<div className="row">
+				{products.map((p) => (
+					<div className="col-4">
+						<div className="card">
+							<img src={p.thumbnail} alt={p.name} />
+							<div className="content">
+								<h2>{p.title}</h2>
+								<p>Gia: {p.price}</p>
+								<button className="btn btn-primary w-100">Mua ngay</button>
+								<button className="btn btn-danger w-100">Xem chi tiet</button>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 }
 
 export default HomePage;
