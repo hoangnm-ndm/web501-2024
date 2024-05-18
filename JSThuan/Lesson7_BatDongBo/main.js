aeb345a477d29170fa344858fc064dd41a1a9ac8
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-18 13:01:39 +0700

    Lesson 6: BT

--- JSThuan/Lesson7_BatDongBo/main.js
+++ JSThuan/Lesson7_BatDongBo/main.js
@@ -2,28 +2,38 @@
  * ! Asynchronous
  */
 
-function xinTien(callback) {
-	setTimeout(() => {
-		console.log("Xin dk tien");
-		callback();
-	}, 5000);
-}
-function muaXe(callback) {
-	setTimeout(() => {
-		console.log("Mua xe may");
-		callback();
-	}, 1500);
-}
+// function xinTien(callback) {
+// 	setTimeout(() => {
+// 		console.log("Xin dk tien");
+// 		callback();
+// 	}, 5000);
+// }
+// function muaXe(callback) {
+// 	setTimeout(() => {
+// 		console.log("Mua xe may");
+// 		callback();
+// 	}, 1500);
+// }
 
-function diHoc() {
-	setTimeout(() => {
-		console.log("Di hoc");
-	}, 3000);
-}
+// function diHoc() {
+// 	setTimeout(() => {
+// 		console.log("Di hoc");
+// 	}, 3000);
+// }
 
 // muaXe(xinTien);
-xinTien(() => {
-	muaXe(() => {
-		diHoc();
-	});
-});
+// xinTien(() => {
+// 	muaXe(() => {
+// 		diHoc();
+// 	});
+// });
+
+fetch("https://dummyjson.com/products")
+	.then((response) => response.json())
+	.then(({ products }) => {
+		console.log(products);
+		sanpham = products;
+	})
+	.catch((error) => console.log(error));
+
+// ! Sau khi lay dk danh sach san pham thi tim cach hien thi ra ngoai html
--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main></main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main></main>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
