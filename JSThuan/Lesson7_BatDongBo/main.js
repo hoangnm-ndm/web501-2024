2374dcd569ecda2296cc4e95aba17bc974f1b32b
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-18 13:49:42 +0700

    Lesson 6: Promise va async/await

--- JSThuan/Lesson7_BatDongBo/main.js
+++ JSThuan/Lesson7_BatDongBo/main.js
@@ -1,39 +1,22 @@
-/**
- * ! Asynchronous
- */
+const productElement = document.getElementById("productList");
+console.log(productElement);
 
-// function xinTien(callback) {
-// 	setTimeout(() => {
-// 		console.log("Xin dk tien");
-// 		callback();
-// 	}, 5000);
-// }
-// function muaXe(callback) {
-// 	setTimeout(() => {
-// 		console.log("Mua xe may");
-// 		callback();
-// 	}, 1500);
-// }
+// fetch("https://dummyjson.com/products")
+// 	.then((response) => response.json())
+// 	.then(({ products }) => {
+// 		console.log(products);
+// 	})
+// 	.catch((error) => console.log(error));
 
-// function diHoc() {
-// 	setTimeout(() => {
-// 		console.log("Di hoc");
-// 	}, 3000);
-// }
-
-// muaXe(xinTien);
-// xinTien(() => {
-// 	muaXe(() => {
-// 		diHoc();
-// 	});
-// });
-
-fetch("https://dummyjson.com/products")
-	.then((response) => response.json())
-	.then(({ products }) => {
+// TODO: BTVN: Sau khi lay dk danh sach san pham thi tim cach hien thi ra ngoai html
+async function fetchAPI() {
+	try {
+		const result = await fetch("https://dummyjson.com/products");
+		const { products } = await result.json();
 		console.log(products);
-		sanpham = products;
-	})
-	.catch((error) => console.log(error));
+	} catch (error) {
+		console.log(error);
+	}
+}
 
-// TODO: Sau khi lay dk danh sach san pham thi tim cach hien thi ra ngoai html
+fetchAPI();
