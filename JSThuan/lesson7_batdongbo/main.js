72ced15f5136c34cc35135d6ba6f2fc1b1ca0897
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-20 11:13:10 +0700

    BTVN

--- JSThuan/lesson7_batdongbo/main.js
+++ JSThuan/lesson7_batdongbo/main.js
@@ -5,9 +5,10 @@ fetch("http://localhost:3000/products")
 	});
 
 // TODO: Lấy danh sách sản phẩm hiển thị lên file index.html với giao diện đầy đủ (ảnh, tên, giá, nút mua hàng)
+// Nếu không cài thành công json-server thì xem lại hướng dẫn trong file notes/Lesson7.json-server.md
 /**
  * Gợi ý:
- * 1. Khởi chạy server với câu lệnh: `npm run server`
- * 2. Tạo 1 element trong html.
- * 3. Sau khi lấy được dự liệu, add dữ liệu vào element ở bước 2 bởi innerHTML
+ * ?1. Khởi chạy server với câu lệnh: `npm run server`
+ * ?2. Tạo 1 element trong html.
+ * ?3. Sau khi lấy được dự liệu, add dữ liệu vào element ở bước 2 bởi innerHTML
  */
--- notes/Lesson7.json-server.md
+++ notes/Lesson7.json-server.md
@@ -4,3 +4,9 @@ Buoc 1: cai dat:
 Buoc 2: tao file db.json va them data fake
 
 Buoc 3: Thay doi script `dev`
+
+```json
+
+	"dev": "concurrently \"vite --port 5173\" \"json-server --watch db.json --port 3000\"",
+	"server": "json-server --watch db.json --port 3000",
+```
