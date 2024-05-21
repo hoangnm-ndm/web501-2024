2d0293b7ce23e70f836c6a3d01163583162e17e8
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-21 15:05:55 +0700

    Lesson 7: Bat dong bo

--- JSThuan/Lesson6_Asynchronous/main.js
+++ JSThuan/Lesson6_Asynchronous/main.js
@@ -1,31 +1,62 @@
-function quaMon(callback) {
-	setTimeout(() => {
-		console.log("TaskA Done");
-		callback();
-	}, 6000);
+function taskA() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			console.log("TaskA Done");
+			resolve();
+		}, 6000);
+	});
 }
 
-function totNghiep(callback) {
-	setTimeout(() => {
-		console.log("TaskB Done");
-		callback();
-	}, 4000);
+function taskB() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			console.log("TaskB Done");
+			resolve();
+		}, 4000);
+	});
 }
 
-function kiemTien(callback) {
-	setTimeout(() => {
-		console.log("TaskC");
-		callback();
-	}, 2000);
+function taskC() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			console.log("TaskC");
+			resolve();
+		}, 2000);
+	});
 }
+
+// Cach 1: Callback
+// taskA(() => {
+// 	taskB(() => {
+// 		taskC(() => {
+// 			console.log("All Done");
+// 		});
+// 	});
+// });
 
 // ! A -> B -> C
-// Cach 1: Callback.
+// Cach 2: Promise.
 
-quaMon(() => {
-	totNghiep(() => {
-		kiemTien(() => {
-			console.log("All Done");
-		});
-	});
-});
+// taskA()
+// 	.then(() => taskB())
+// 	.then(() => taskC())
+// 	.then(() => {
+// 		console.log("All Done");
+// 	})
+// 	.catch((error) => {
+// 		console.log(error);
+// 	});
+
+// Cach 3: Async/Await
+async function runTask() {
+	try {
+		await taskA();
+		await taskB();
+		await taskC();
+		console.log("Xong het!");
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+// runTask();
