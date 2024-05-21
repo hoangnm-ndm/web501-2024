ae335559330e3fea7cf429718b64178d4aba8295
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-21 12:50:15 +0700

    Lesson 7: Trang danh sách

--- JSThuan/Lesson7_BatDongBo/main.js
+++ JSThuan/Lesson7_BatDongBo/main.js
@@ -1,22 +1,30 @@
 const productElement = document.getElementById("productList");
-console.log(productElement);
 
-// fetch("https://dummyjson.com/products")
-// 	.then((response) => response.json())
-// 	.then(({ products }) => {
-// 		console.log(products);
-// 	})
-// 	.catch((error) => console.log(error));
+fetch("https://dummyjson.com/products")
+	.then((response) => response.json())
+	.then(({ products }) => {
+		products.forEach((item) => {
+			const productItem = document.createElement("div");
+			productItem.innerHTML = `
+				<img src="${item.thumbnail}" alt="${item.title}" />
+				<h3>${item.title}</h3>
+				<p>${item.price}</p>
+				<button className="btn">Add to card</button>
+			`;
+			productElement.appendChild(productItem);
+		});
+	})
+	.catch((error) => console.log(error));
 
 // TODO: BTVN: Sau khi lay dk danh sach san pham thi tim cach hien thi ra ngoai html
-async function fetchAPI() {
-	try {
-		const result = await fetch("https://dummyjson.com/products");
-		const { products } = await result.json();
-		console.log(products);
-	} catch (error) {
-		console.log(error);
-	}
-}
+// async function fetchAPI() {
+// 	try {
+// 		const result = await fetch("https://dummyjson.com/products");
+// 		const { products } = await result.json();
+// 		console.log(products);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// }
 
-fetchAPI();
+// fetchAPI();
--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,37 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+const products = [
+	{ id: 1, name: "Product 1", price: 100 },
+	{ id: 2, name: "Product 2", price: 200 },
+	{ id: 3, name: "Product 3", price: 300 },
+];
+
+const HomePage = () => {
+	return (
+		<>
+			{products.map((item) => (
+				<div key={item.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+					<div className="card">
+						<img src={item.thumbnail} alt={item.name} />
+						<div className="content">
+							<h2>{item.name}</h2>
+							<p>Price: {item.price}</p>
+							<button className="btn btn-danger w-100">Add to card</button>
+						</div>
+					</div>
+				</div>
+			))}
+		</>
+	);
+};
 function App() {
 	return (
 		<>
 			<Header />
-			<main></main>
+			<main>
+				<HomePage />
+			</main>
 			<Footer />
 		</>
 	);
