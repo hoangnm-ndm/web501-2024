e00356c0151142de4c2ff23973b8a472e0006e45
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-21 13:14:51 +0700

    Lesson 7: Props

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,16 @@ const products = [
 	{ id: 3, name: "Product 3", price: 300 },
 ];
 
-const HomePage = () => {
+const products2 = [
+	{ id: 1, name: "Product A", price: 100 },
+	{ id: 2, name: "Product B", price: 200 },
+	{ id: 3, name: "Product C", price: 300 },
+];
+
+const HomePage = (props) => {
 	return (
 		<>
-			{products.map((item) => (
+			{props.data.map((item) => (
 				<div key={item.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
 					<div className="card">
 						<img src={item.thumbnail} alt={item.name} />
@@ -27,12 +33,16 @@ const HomePage = () => {
 		</>
 	);
 };
+
+// ! props = properties
 function App() {
 	return (
 		<>
 			<Header />
 			<main>
-				<HomePage />
+				{/* {HomePage(products)} */}
+				<HomePage data={products} />
+				<HomePage data={products2} />
 			</main>
 			<Footer />
 		</>
