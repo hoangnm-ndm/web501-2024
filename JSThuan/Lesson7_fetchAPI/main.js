7c3fb1eabb1ff54e615c9d7c0aa3379ee41ef3e5
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-22 09:39:39 +0700

    Lesson 7: fetch API

--- JSThuan/Lesson7_fetchAPI/main.js
+++ JSThuan/Lesson7_fetchAPI/main.js
@@ -2,7 +2,21 @@ const productsListElement = document.getElementById("productsList");
 
 fetch("https://dummyjson.com/products")
 	.then((response) => response.json())
-	.then((data) => console.log(data))
-	.catch(console.log(error));
+	.then(({ products }) => {
+		console.log(products);
 
+		const content = products
+			.map((item) => {
+				return `
+				<div>
+					<h2>${item.title}</h2>
+				</div>
+			`;
+			})
+			.join("");
+		productsListElement.innerHTML = content;
+	})
+	.catch((error) => {
+		console.log(error);
+	});
 // TODO: Lấy danh sách sản phẩm hiển thị lên html
