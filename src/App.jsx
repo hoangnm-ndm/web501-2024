0d4dc4ffe37a6e7ea0cdb2c022e41f737e843116
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-23 14:44:03 +0700

    Lesson 8: state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -9,16 +9,43 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		fetch("https://dummyjson.com/products")
+			.then((response) => response.json())
+			.then(({ products }) => {
+				console.log(products);
+				setProducts(products);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 	return (
 		<div>
 			<Header />
 			<main className="container">
-				<Routes>
+				<div className="row">
+					{products.map((item) => (
+						<div className="col-12 col-sm-6 col-md-4 col-lg-3 item" key={item.id}>
+							<div>
+								<img src={item.thumbnail} alt={item.title} />
+							</div>
+							<div className="content">
+								<h2>{item.title}</h2>
+								<p>Gia: {item.price}</p>
+								<button className="btn btn-danger">Xem chi tiet</button>
+							</div>
+						</div>
+					))}
+				</div>
+
+				{/* <Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/about" element={<About />} />
 					<Route path="*" element={<NotFound />} />
-				</Routes>
+				</Routes> */}
 			</main>
 			<Footer />
 		</div>
