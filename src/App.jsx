f8cfb83caaf498c18b524a71aa47cdd45c3393ff
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-25 12:35:55 +0700

    lesson 9: danh sach san pham

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -7,13 +7,24 @@ import Home from "./pages/Home";
 // ! props = properties
 function App() {
 	const [products, setProducts] = useState([]);
-	useEffect(() => {}, []);
+	useEffect(() => {
+		async function fetchAPI() {
+			try {
+				const res = await fetch("http://localhost:3000/products");
+				const data = await res.json();
+				setProducts(data);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchAPI();
+	}, []);
 
 	return (
 		<>
 			<Header />
-			<main>
-				<Home />
+			<main className="container">
+				<Home data={products} />
 			</main>
 			<Footer />
 		</>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
-const Home = () => {
-	return <></>;
+const Home = ({ data }) => {
+	console.log(data);
+	return (
+		<>
+			<div className="row">
+				{data.map((p) => (
+					<div className="col-12 col-sm-6 col-md-4 col-lg-3">
+						<div className="card">
+							<img src={p.thumbnail} alt="" />
+							<div className="content">
+								<h2>{p.title}</h2>
+								<p>Price: {p.price}</p>
+								<p>Brand: {p.brand}</p>
+								<button className="btn btn-danger w-100">Add to card</button>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default Home;
