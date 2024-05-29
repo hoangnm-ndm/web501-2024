1a9d668d1f6bc01d4bd627aefd68fc18c76dcf54
hoangnm <hoangnm.ndm@gmail.com>
2024-05-29 11:02:36 +0700

    Lesson 11: fix form

--- src/pages/admin/ProductAdd.jsx
+++ src/pages/admin/ProductAdd.jsx
@@ -5,9 +5,10 @@ import * as z from "zod";
 
 const schemaProduct = z.object({
 	title: z.string().min(3),
-	price: z.number().min(0),
+	price: z.number().min(1),
 	description: z.string(),
 });
+
 const ProductAdd = () => {
 	const {
 		register,
@@ -25,7 +26,7 @@ const ProductAdd = () => {
 				<h1>Add product</h1>
 				<div className="form-group mb-3">
 					<label className="form-label" htmlFor="title">
-						Title
+						Titlecleqr
 					</label>
 					<input className="form-control" type="text" id="title" {...register("title", { required: true })} />
 					{errors.title?.message && <p className="text-danger">{errors.title?.message}</p>}
@@ -35,14 +36,20 @@ const ProductAdd = () => {
 					<label className="form-label" htmlFor="price">
 						Price
 					</label>
-					<input className="form-control" type="number" id="price" {...register("price", { required: true })} />
+					<input className="form-control" type="number" id="price" {...register("price", { valueAsNumber: true })} />
 					{errors.price?.message && <p className="text-danger">{errors.price?.message}</p>}
 				</div>
 				<div className="form-group mb-3">
 					<label className="form-label" htmlFor="description">
 						Description
 					</label>
-					<input className="form-control" type="text" id="description" {...register("price", { required: true })} />
+					<input
+						className="form-control"
+						type="text"
+						id="description"
+						{...register("description", { required: true })}
+					/>
+					{errors.description?.message && <p className="text-danger">{errors.title?.message}</p>}
 				</div>
 				<div className="form-group mb-3">
 					<button className="btn btn-primary w-100" type="submit">
